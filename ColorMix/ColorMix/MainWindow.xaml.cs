0c751fd1c63bc985c2970d7e429de2bd6a3ee529
pfefferberh <pfefferberhorion@gmail.com>
2020-05-27 22:06:57 +0300

    binding

---
+++ ColorMix/ColorMix/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ColorMix
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Label> labelsNam = new List<Label>();
+        List<Label> labels = new List<Label>();
+        int memb = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            labels.Add(Lb1);
+            labels.Add(Lb2);
+            labels.Add(Lb3);
+            labels.Add(Lb4);
+            labels.Add(Lb5);
+            labelsNam.Add(LbNam1);
+            labelsNam.Add(LbNam2);
+            labelsNam.Add(LbNam3);
+            labelsNam.Add(LbNam4);
+            labelsNam.Add(LbNam5);
+        }
+        private void Check()
+        {
+            byte memb1 = 0;
+            byte memb2 = 0;
+            byte memb3 = 0;
+            byte memb4 = 0;
+            if (CbAlpha.IsChecked == true) memb1 = (byte)SAlpha.Value;
+            else memb1 = 0;
+            if (CbRed.IsChecked == true) memb2 = (byte)SRed.Value;
+            else memb2 = 0;
+            if (CbGreen.IsChecked == true) memb3 = (byte)SGreen.Value;
+            else memb3 = 0;
+            if (CbBlue.IsChecked == true) memb4 = (byte)SBlue.Value;
+            else memb4 = 0;
+            ColBut.Background = new SolidColorBrush(Color.FromArgb(memb1, memb2, memb3, memb4));
+        }
+        private void S_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Check();
+        }
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Check();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int perevir = labelsNam.Count;
+            if (memb == labelsNam.Count)
+                memb = 0;
+            for (int j = 0; j < labelsNam.Count; j++)
+                if (ColBut.Background.ToString() != labelsNam[j].Content.ToString())
+                    perevir--;
+            if (perevir == 0)
+            {
+                for (int j = 0; j < labelsNam.Count; j++)
+                    if (labelsNam[j].Content.ToString() == "Del")
+                    {
+                        labels[j].Background = ColBut.Background;
+                        labelsNam[j].Content = ColBut.Background.ToString();
+                        break;
+                    }
+                    else if(j == labelsNam.Count-1)
+                    {
+                        labels[memb].Background = ColBut.Background;
+                        labelsNam[memb].Content = ColBut.Background.ToString();
+                        memb++;
+                    }
+            }
+        }
+        private void Button_Click_Del(object sender, RoutedEventArgs e)
+        {
+            Button button= sender as Button;
+            button.Tag = "Del";
+            button.Background = new SolidColorBrush(Colors.White);
+        }
+    }
+}
+
---
+++ KeyboardTrainer/KeyboardTrainer/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace KeyboardTrainer
+{
+    internal sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            for(int i=0;i<15;i++)
+            OutText.Text += Convert.ToChar(random.Next(97, 122));
+            //PreviewKeyDown += MainWindow_PreviewKeyDown;
+            // KeyDown += MainWindow_KeyDown;
+            PreviewTextInput += TextBlock_PreviewTextInput;
+        }
+
+        private void MainWindow_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+          // (sender as Grid).Children[0].GotKeyboardFocus
+            //InText.Text +=
+            //Klava_row1.Children[0].;
+
+
+        }
+
+        private void MainWindow_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            //InText.Text += e.;
+        }
+
+        private void MainWindow_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+          //  InText.Text += e.;
+        }
+
+        private void TextBlock_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            InText.Text += e.Text;
+           // InText.Text += ;
+            //if ((sender as TextBlock).Text == e.Text)
+            //    (sender as TextBlock).Background = new SolidColorBrush(Colors.White);
+        }
+    }
+}
