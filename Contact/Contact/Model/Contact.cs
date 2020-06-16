5cd86607818ec46c99751be4f79ccbaeb237f0ca
pfefferberh <pfefferberhorion@gmail.com>
2020-06-16 15:51:08 +0300

    i dont understant this topic

---
+++ Contact/Contact/Model/Contact.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Contact.Model
+{
+    public class Contacts : INotifyPropertyChanged
+    {
+        private string name;
+        private string surname;
+        private int number;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Surname
+        {
+            get => surname;
+            set
+            {
+                surname = value;
+                OnPropertyChanged("Surname");
+            }
+        }
+        public int Number
+        {
+            get => number;
+            set
+            {
+                number = value;
+                OnPropertyChanged("Number");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
+
---
+++ Contact/Contact/ViewModel/ApplicationViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Contact.Model;
+using Contact.ViewModel.Comand;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Contact.ViewModel
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Contacts selectedContacts;
+        private Contacts contact; 
+        public ObservableCollection<Contacts> Cnts { get; set; }
+        public Contacts SelectedContacts
+        {
+            get { return selectedContacts; }
+            set
+            {
+                selectedContacts = value;
+                OnPropertyChanged("SelectedContacts");
+            }
+        }
+        public Contacts Contact
+        {
+            get { return contact; }
+            set
+            {
+                contact = value;
+                OnPropertyChanged("Contact");
+            }
+        }
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      Contacts phone = new Contacts();
+                      Cnts.Add(contact);
+                      SelectedContacts = phone;
+                  }));
+            }
+        }
+  
+   
+
+        public ApplicationViewModel()
+        {
+            Cnts = new ObservableCollection<Contacts>
+            {
+                new Contacts { Name="iPhone 7", Surname="Apple", Number=56000 },
+                new Contacts {Name="Galaxy S7 Edge", Surname="Samsung", Number =60000 }
+
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
