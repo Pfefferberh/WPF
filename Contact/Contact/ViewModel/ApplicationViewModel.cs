cf12b4dd5c403e4810c1565e41eefd944f5d0d9f
pfefferberh <pfefferberhorion@gmail.com>
2020-06-19 00:29:44 +0300

    its haard do task about colaps

--- Contact/Contact/ViewModel/ApplicationViewModel.cs
+++ Contact/Contact/ViewModel/ApplicationViewModel.cs
@@ -9,14 +9,32 @@ namespace Contact.ViewModel
     public class ApplicationViewModel : INotifyPropertyChanged
     {
         private Contacts selectedContacts;
-        private Contacts contact; 
+        private Contacts contact;
+        public string visibl_edit { get; set; } = "Collapsed";
         public ObservableCollection<Contacts> Cnts { get; set; }
+        public ApplicationViewModel()
+        {
+            Cnts = new ObservableCollection<Contacts>
+            {
+                new Contacts { Name="iPhone 7", Surname="Apple", Number=56000 },
+                new Contacts {Name="Galaxy S7 Edge", Surname="Samsung", Number =60000 }
+
+            };
+            Contact = new Contacts();
+            SelectedContacts = new Contacts();
+        }
         public Contacts SelectedContacts
         {
             get { return selectedContacts; }
             set
             {
                 selectedContacts = value;
+                Contact = new Contacts
+                {
+                    Name = selectedContacts.Name,
+                    Number = selectedContacts.Number,
+                    Surname = selectedContacts.Surname
+                };
                 OnPropertyChanged("SelectedContacts");
             }
         }
@@ -29,6 +47,7 @@ namespace Contact.ViewModel
                 OnPropertyChanged("Contact");
             }
         }
+       
         private RelayCommand addCommand;
         public RelayCommand AddCommand
         {
@@ -37,23 +56,21 @@ namespace Contact.ViewModel
                 return addCommand ??
                   (addCommand = new RelayCommand(obj =>
                   {
-                      Contacts phone = new Contacts();
-                      Cnts.Add(contact);
-                      SelectedContacts = phone;
+                      Contacts phone = new Contacts()
+                      {
+                          Name = Contact.Name,
+                          Number = Contact.Number,
+                          Surname = Contact.Surname,
+                      };
+                      Cnts.Add(phone);
+                  }, obj =>
+                  {
+                      if (obj as Contacts != null)
+                          if (!string.IsNullOrEmpty((obj as Contacts).Name) && !string.IsNullOrEmpty((obj as Contacts).Surname))
+                              return true;
+                      return false;
                   }));
             }
-        }
-  
-   
-
-        public ApplicationViewModel()
-        {
-            Cnts = new ObservableCollection<Contacts>
-            {
-                new Contacts { Name="iPhone 7", Surname="Apple", Number=56000 },
-                new Contacts {Name="Galaxy S7 Edge", Surname="Samsung", Number =60000 }
-
-            };
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
--- Contact/Contact/ViewModel/Comand/RelayCommand.cs
+++ Contact/Contact/ViewModel/Comand/RelayCommand.cs
@@ -5,8 +5,31 @@ namespace Contact.ViewModel.Comand
 {
     public class RelayCommand : ICommand
     {
-        private Action<object> execute;
-        private Func<object, bool> canExecute;
+        private Action<object> execute;  // void NameMethod(0...16args)
+     
+        /*
+         void Execute(object param){
+            cw("hello world");
+        }
+
+
+        execute = Execute;
+        execute?.Invoke("something");
+        ==
+        execute("something");
+         */
+        
+        private Func<object, bool> canExecute;  // bool NameMethod(0..16args)
+        /*
+         bool CanDo(object param){
+        return true;
+        }
+
+        canExecute = CanDo;
+        canExecute = ()=>true;
+
+        canExecute("any object");
+         */
 
         public event EventHandler CanExecuteChanged
         {
